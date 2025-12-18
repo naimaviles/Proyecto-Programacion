5336917f5af2963f9215080998157197658d5478
Naïma Aviles <naimaaviles@Naimas-MacBook-Air.local>
2025-12-18 09:27:05 +0100

    He cambiado el nombre del archivo y he empezado con la ventana

---
+++ Ventana.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import os
+
+#VENTANTA PRINCIPAL
+ventana = tk.Tk()
+ventana.title("Generador de Contraseñas")
+ventana.geometry("400x300")
+BG_COLOR = "#AEB0EA"
+ventana.config(bg=BG_COLOR)
+
+
