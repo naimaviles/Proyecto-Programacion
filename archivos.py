2fd3e6ce4b88c528b97e51de4e215f6e1539cf1c
naimaviles <naimapufis@gmail.com>
2026-01-08 20:11:36 +0100

    Fix indentation and formatting in archivos.py

--- archivos.py
+++ archivos.py
@@ -1,10 +1,11 @@
-# Guardar y leer la contraseña en un archivo de texto.
+#Guardar y leer la contraseña en un archivo de texto.
 def guardar_password(nombre_archivo, password):
-    # "w" = escribir (sobrescribe el archivo)
+    #"w" = escribir (sobrescribe el archivo)
     with open(nombre_archivo, "w", encoding="utf-8") as f:
         f.write(password)
 
 def leer_password(nombre_archivo):
-    # "r" = leer
+    #"r" = leer
     with open(nombre_archivo, "r", encoding="utf-8") as f:
         return f.read().strip()
+
